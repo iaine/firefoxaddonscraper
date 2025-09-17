a51d307d593237c1fb2eab27008b5259b69f79ae
Iain Emsley <Iain.Emsley@warwick.ac.uk>
2025-09-17 08:31:33 +0100

    Merge of testing fixes and rename

--- scraper.py
+++ scraper.py
@@ -6,12 +6,11 @@
 import re
 import ssl 
 from urllib.request import urlopen
-
+import json
 from bs4 import BeautifulSoup
 
 import pandas as pd
 
-import ScraperException
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
@@ -26,20 +25,50 @@ class FFScraper():
         if userid == "":
             raise ScraperException("No user given.")
         
-        base = f"https://addons.mozilla.org/{language}/firefox/user/{userid}/"
-        data = urlopen(base)
-        #print(base)
+        base = f"https://addons.mozilla.org/api/v5/addons/search/?app=firefox&author={userid}&page=1&page_size=10&sort=users&type=extension&lang={language}"
+        data = urlopen(base, timeout=10)
+        results = json.loads(data.read().decode('utf-8'))
 
-        # parsing the html file
-        htmlParse = BeautifulSoup(data, 'html.parser')
-        print(htmlParse)
-        links = []
-        for link in htmlParse.find_all('ul', {'class': 'AddonsCard-list'}):
-            for li in link.find_all('a', href=True):
-                links.append(li)
-        return links
+        appresults = []
+        for result in results['results']:
+            app = {}
+            authornames = []
+            authorurls = []
+            for author in result['authors']:
+                authornames.append(author['name'])
+                authorurls.append(author['url'])
+            app['authornames'] = ";".join(authornames)
+            app['authorurls'] = ";".join(authorurls)
+            app['categories'] = ";".join(result['categories'])
+            app['url'] = result['current_version']['file']['url']
+            app['permissions'] = ";".join(result['current_version']['file']['permissions'])
+            app['optpermissions'] = ";".join(result['current_version']['file']['optional_permissions'])
+            app['hostpermissions'] = ";".join(result['current_version']['file']['host_permissions'])
+            app['datapermissions'] = ";".join(result['current_version']['file']['data_collection_permissions'])
+            app['licenseurl'] = result['current_version']['license']['url']
+            app['releasenotes'] = result['current_version']['release_notes']
+            app['privacy'] = result['has_privacy_policy']
+            keys = list(result['description'].keys())
+            app['description'] = result['description'][keys[0]]
+            keys = list(result['homepage']['url'].keys())
+            app['home'] = result['homepage']['url'][keys[0]]
+            keys = list(result['name'].keys())
+            app['name'] = result['name'][keys[0]]
+            app['last'] = result['last_updated']
+            app['ratingscount'] = result['ratings']['average']
+            app['ratingsnumber'] = result['ratings']['count']
+            app['ratingstext'] = result['ratings']['text_count']
+            keys = list(result['summary'].keys())
+            app['summary'] = result['summary'][keys[0]]
+            app['tags'] = ";".join(result['tags'])
+            app['versions'] = result['versions_url']
+            app['downloads'] = result['weekly_downloads']
 
-    def find_categories(self, language=""):
+            appresults.append(app)
+            
+        return appresults
+
+    def find_categories(self, addontype, language=""):
         '''
             Get Categories
         '''
@@ -47,7 +76,7 @@ class FFScraper():
         if language == "":
             raise ScraperException("No language given. Please use a code like en-GB or cn-ZH")
         
-        base = f"https://addons.mozilla.org/{language}/firefox/extensions/"
+        base = f"https://addons.mozilla.org/{language}/firefox/{addontype}/"
         data = urlopen(base)
 
         # parsing the html file
@@ -132,25 +161,38 @@ class FFScraper():
         '''
         links = []
 
-        data = urlopen(url + '/version', timeout=10)
+        data = urlopen(url + '/versions', timeout=10)
 
         htmlParse = BeautifulSoup(data, 'html.parser')
-        history = {}
+        
         for lnk in htmlParse.find_all("li", {'class': 'AddonVersionCard'}):
-            history['version'] = lnk.find('div', {'class': 'AddonVersionCard-version'})
+            history = {}
+            history['version'] = lnk.find('h2', {'class': 'AddonVersionCard-version'}).get_text()
             release = lnk.find('div', {'class': 'AddonVersionCard-fileInfo'}).get_text().split(' - ')
             history['releaseDate'] = release[0]
             history['releaseSize'] = release[1]
-            history['platform'] = lnk.find('div', {'class': 'AddonVersionCard-compatability'})
+            history['platform'] = lnk.find('div', {'class': 'AddonVersionCard-compatibility'}).get_text()
             note = lnk.find('div', {'class': 'AddonVersionCard-releaseNotes'})
-            history['notes'] = note.get_text()
-            history['noteslink'] = note.find('a')['href']
-            history['license'] = lnk.find('div', {'class': 'AddonVersionCard-license'}).get_text()
-            history['download'] = lnk.find('div', {'class': 'InstallButtonWrapper-download'}).find('a')['href']
+            if note is not None:
+                history['notes'] = note.get_text()
+            else:
+                history['notes'] = ""
+            if note.find('a') is not None:
+                history['noteslink'] = note.find('a').get('href')
+            else:
+                history['noteslink'] = ""
+            
+            license = lnk.find('div', {'class': 'AddonVersionCard-license'})
+            if license is not None:
+                history['license'] = license.get_text()
+            else:
+                history['license'] = ""
+            history['download'] = lnk.find('div', {'class': 'InstallButtonWrapper-download'}).find('a').get('href')
             
             links.append(history)
 
         return links
+
 
     def get_link_details(self, url):
         '''
@@ -176,17 +218,13 @@ class FFScraper():
         else:
             details['description'] = ""
         
-        #details['badges'] = htmlParser.find('span',{'class':'AddonTitle-author'})
         if htmlParser.find('h4',{'class':'PermissionsCard-subhead--required'}) != None:
             reqs = []
             for req in htmlParser.find_all('li',{'class':'Permission'}): reqs.append(req.get_text())
             details['requiredpermissions'] = ";".join(reqs)
         else:
             details['requiredpermissions'] = ""
-        #lnks = htmlParser.find('dd',{'class':'AddonMoreInfo-links'})
-        #print(lnks)
-        #ls = [l['href'] for l in lnks.find_all('a')]
-        #details['links'] = ";".join(ls)
+        
         details['version'] = htmlParser.find('dd',{'class':'AddonMoreInfo-version'}).get_text()
         details['size'] = htmlParser.find('dd',{'class':'AddonMoreInfo-filesize'}).get_text()
         details['last'] = htmlParser.find('dd',{'class':'AddonMoreInfo-last-updated'}).get_text()
@@ -202,8 +240,12 @@ class FFScraper():
             details['privacy'] = priv.find('a').get('href')
         else:
             details['privacy'] = ""
-        #details['tags'] = htmlParser.find('dd',{'class':'AddonMoreInfo-tag_links'}).get_text()
-
+            
+        history = htmlParser.find('dd',{'class':'AddonMoreInfo-version-history'})
+        if history is not None:
+            details['history'] = history.find('a').get('href')
+        else:
+            details['history'] = ""
         return details
 
     def get_links_details(self, linksarray):
@@ -212,7 +254,6 @@ class FFScraper():
         '''
         details = []
         for l in linksarray:
-            print(l)
             details.append(self.get_link_details(l))
         return details
 
